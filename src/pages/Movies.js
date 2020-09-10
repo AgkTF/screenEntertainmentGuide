77935f68bbe604aec9f6a9c1caf24cff9d7e3b6e
agkz1st <ahmed.agk22@gmail.com>
2020-09-11 01:18:45 +0200

    a little pause.

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import MovieCarousel from "../components/MovieCarousel/MovieCarousel";
 const Movies = () => {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+
+      <MovieCarousel />
+    </>
+  );
 };
 
 export default Movies;
