d8b40f5a4694cca830e8a7018d3f1c6068643a6e
agkz1st <ahmed.agk22@gmail.com>
2020-09-11 03:41:07 +0200

    MovieCarousel component now works fine. at least the images part after modifying the css files of slick-carousel package. Next is extracting all the css from it and scope them to the component then test on different screen sizes.

--- src/components/MovieCarousel/MovieCarousel.js
+++ src/components/MovieCarousel/MovieCarousel.js
@@ -1,41 +1,81 @@
 import React from "react";
+import classes from "./MovieCarousel.module.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const MovieCarousel = () => {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 1,
-    speed: 500,
-  };
-  return (
-    <div className="w-64 mx-auto my-1">
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
-  );
-};
+class MovieCarousel extends React.Component {
+  render() {
+    const settings = {
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "48px",
+      slidesToShow: 1,
+      speed: 500,
+      arrows: false,
+    };
+    return (
+      <div className="mx-auto my-1" style={{ width: "320px", height: "320px" }}>
+        <Slider {...settings}>
+          <div
+            className={`bg-pink-300 rounded-lg overflow-hidden ${classes.imgContainer}`}
+          >
+            <img
+              src="images/1.jpg"
+              alt="dummy"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div
+            className={`bg-pink-300 rounded-lg overflow-hidden ${classes.imgContainer}`}
+          >
+            <img
+              src="images/2.jpg"
+              alt="dummy"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div
+            className={`bg-pink-300 rounded-lg overflow-hidden ${classes.imgContainer}`}
+          >
+            <img
+              src="images/3.jpg"
+              alt="dummy"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div
+            className={`bg-pink-300 rounded-lg overflow-hidden ${classes.imgContainer}`}
+          >
+            <img
+              src="images/4.jpg"
+              alt="dummy"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div
+            className={`bg-pink-300 rounded-lg overflow-hidden ${classes.imgContainer}`}
+          >
+            <img
+              src="images/5.jpg"
+              alt="dummy"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div
+            className={`bg-pink-300 rounded-lg overflow-hidden ${classes.imgContainer}`}
+          >
+            <img
+              src="images/6.jpg"
+              alt="dummy"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
 
 export default MovieCarousel;
